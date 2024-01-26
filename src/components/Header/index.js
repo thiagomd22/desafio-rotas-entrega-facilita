69f66f92aa6567fe654bce5079a345fb53b335d3
Thiago Meireles <thiago22md@gmail.com>
2024-01-26 16:07:53 -0300

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+
+export default function Header() {
+  return(
+    <header>
+      <div className="header">
+        <div className="titulo">ReactLimp</div>
+        <div className="subtitulo">Aqui tudo fica limpo</div>
+      </div>
+    </header>
+  )
+}
